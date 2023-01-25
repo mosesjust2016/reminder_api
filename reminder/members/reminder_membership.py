2e3fcc87907d82de380194cbaecd20d2865bd918
Moses Jasi <moses@getsparco.com>
2023-01-25 12:36:09 +0200

    Fixed a Response bug

--- reminder/members/reminder_membership.py
+++ reminder/members/reminder_membership.py
@@ -5,7 +5,7 @@ from .. models.membersModel import Member
 from .. function import convert_phone_number, sendFile_wa_GAPI, sendFile_wa_GAPI_group, send_wa_GreenAPI
 from datetime import date, datetime, timedelta
 from extensions import db
-#from flask_recaptcha import ReCaptcha # Import ReCaptcha object
+from flask_recaptcha import ReCaptcha # Import ReCaptcha object
 import pytz, random, json, jwt
 import requests
 import openpyxl
@@ -21,7 +21,7 @@ reminder_membership = Blueprint("reminder_membership", __name__, static_folder="
 base_url = config('BASE_URL')
 current_time = datetime.now(pytz.timezone('Africa/Lusaka'))  
 project_enviroment  = config('ENVIROMENT')
-#recaptcha = ReCaptcha()
+recaptcha = ReCaptcha()
 
 
 @reminder_membership.route("/registration", methods=['POST'])
@@ -45,11 +45,13 @@ def registration():
         if is_user_mobile:
             resp = jsonify({'status': 400,
                             'isError': 'true',  
-                            'message' : 'Mobile Number has already been used to register an account.'}), 200
+                            'message' : 'Mobile Number has already been used to register an account.'}), 400
             return resp
         else:
 
-          
+            if request.method == 'POST': # Check to see if flask.request.method is POST
+                if recaptcha.verify(): # Use verify() method to see if ReCaptcha is filled out
+
                         created_date = current_time.strftime('%Y-%m-%d %H:%M:%S')
                         token = jwt.encode({'id': _wanumber, 'exp' : datetime.utcnow() + timedelta(minutes=60)}, config('SECRET_KEY'), algorithm='HS256') 
 
@@ -59,23 +61,37 @@ def registration():
                             db.session.add(members)
                             db.session.commit()
 
-                            #return redirect(url_for('base_url + "/success.html', vid=token, **request.args) , code=302)
-                            return redirect(url_for("https://reminder.mosesjasi.tk/success.html") , code=302)
+                            resp = jsonify({'status': 200,
+                                            'isError': 'false',
+                                            'message' : registered_members}), 200
+                            return resp
 
                         else:
                             members = Member(firstname = _firstname, lastname = _lastname, wa_number = convert_phone_number(_wanumber), dob= _dob, accepted_terms = False, created_at = created_date)
                             db.session.add(members)
                             db.session.commit()
 
-                            #return redirect(url_for('base_url + "/success.html', vid=token, **request.args) , code=302)
-                            return redirect(url_for("https://reminder.mosesjasi.tk/success.html") , code=302)
-                
+                            resp = jsonify({'status': 200,
+                                            'isError': 'false',
+                                            'message' : registered_members}), 200
+                            return resp
+                    
+                else:
+                    resp = jsonify({'status': 400,
+                            'isError': 'true',
+                            'message' : 'Please fill out the ReCaptcha!'}), 400
+                    return resp
+            else:
+                    resp = jsonify({'status': 400,
+                            'isError': 'true',
+                            'message' : 'Method not allowed'}), 400
+                    return resp
 
      # If the keys are not passed
     else:
         resp = jsonify({'status': 400,
                         'isError': 'true',
-                        'message' : 'One or more key values are missing, please enter missing values'}), 200
+                        'message' : 'One or more key values are missing, please enter missing values'}), 400
         return resp
 
 
@@ -124,7 +140,7 @@ def download_notes():
         JsonString = json.loads(res)
 
         resp = jsonify({'status': 200,
-                        'isError': 'true',
+                        'isError': 'false',
                         'message' : registered_members}), 200
         return resp
 
@@ -154,7 +170,7 @@ def happy_birthday():
                 sleep(randint(3,12))
         
             resp = jsonify({'status': 200,
-                            'isError': 'true',
+                            'isError': 'false',
                             'message' : registered_members}), 200
             return resp
 
@@ -207,8 +223,8 @@ def reading_reminder():
                 sleep(randint(3,12))
             
             resp = jsonify({'status': 200,
-                        'isError': 'true',
-                        'message' : registered_members}), 200
+                            'isError': 'false',
+                            'message' : registered_members}), 200
             return resp
 
 
